285574cf4ac69e38e0f4329fab38c6733dc19598
Ericw154 <ericw154@nycstudents.net>
2024-10-01 13:28:59 +0000

    first commit

---
+++ Scoreboard.java
@@ -0,0 +1,12 @@
+public class Scoreboard
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam;
+}
+public String getScore()
+{
+    return null;
+}
