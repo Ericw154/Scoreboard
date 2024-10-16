26d1c66521b43632df8392a00a422a4acc5565ba
Ericw154 <ericw154@nycstudents.net>
2024-10-16 13:08:50 +0000

    done

--- Scoreboard.java
+++ Scoreboard.java
@@ -5,8 +5,35 @@ public class Scoreboard
     private int teamOneScore;
     private int teamTwoScore;
     private String activeTeam;
+
+
+public Scoreboard(String one, String two)
+{
+    teamOne = one;
+    teamTwo = two;
+    activeTeam = teamOne;
+    
 }
+
 public String getScore()
 {
-    return null;
+    return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+}
+
+public void recordPlay(int score)
+{
+    if(teamOne.equals(activeTeam)){
+        teamOneScore += score;
+    }
+    if(teamTwo.equals(activeTeam)){
+        teamTwoScore += score;
+    }
+    if(score == 0)
+    {
+        if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+        else if(activeTeam.equals(teamTwo)) activeTeam = teamOne;
+    }
+
+}
+
 }
